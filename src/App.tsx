e7d34319660ef01a370dadfd8cb499c5cd1aa9c7
ThePaavero <pekka@astudios.org>
2024-06-29 22:29:32 +0300

    Splitting shit up.

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,10 @@ import './App.scss'
 import MainMenu from './components/MainMenu'
 import Canvas from './components/Canvas'
 import { PixelArray, PotatoEvent, Size } from './mainTypes'
-
-const canvasSizeTemplates = {
-  DEF: { w: 25, h: 25 },
-  NES: { w: 256, h: 240 },
-  MEGADRIVE: { w: 320, h: 224 },
-}
+import resolutionTemplates from './resolutionTemplates'
 
 const App = (): React.JSX.Element => {
-  const [canvasSize] = useState<Size>(canvasSizeTemplates.DEF)
+  const [canvasSize] = useState<Size>(resolutionTemplates.DEF)
   const [history, setHistory] = useState<PotatoEvent[]>([])
 
   const addToHistoryEvents = (potatoEvent: PotatoEvent) => {
@@ -20,12 +15,7 @@ const App = (): React.JSX.Element => {
     setHistory(updatedEvents)
   }
 
-  const canvasData: PixelArray = [
-    {
-      coords: { x: 10, y: 10 },
-      rgba: { R: 0, G: 0, B: 0, A: 100 },
-    },
-  ]
+  const canvasData: PixelArray = []
 
   return (
     <div className="App">
---
+++ src/resolutionTemplates.ts
@@ -0,0 +1,7 @@
+const canvasSizeTemplates = {
+  DEF: { w: 25, h: 25 },
+  NES: { w: 256, h: 240 },
+  MEGADRIVE: { w: 320, h: 224 },
+}
+
+export default canvasSizeTemplates
